8189326a676a8fae594d84a76e5921772fb8e6d5
huichen8 <291222536@qq.com>
2023-08-16 17:49:41 +0800

    add equaler

--- list/list.go
+++ list/list.go
@@ -4,5 +4,8 @@ package list
 // The result will be 0 if a == b, -1 if a < b, and +1 if a > b.
 type Comparator[T any] func(i, j T) int
 
-// LessFn reports whether the element should sort before the other.
+// Equaler reports whether then element equals the other or not.
+type Equaler[T any] func(i, j T) bool
+
+// Less reports whether the element should sort before the other.
 type Less[T any] func(i, j T) bool
