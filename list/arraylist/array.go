33c3c75fc20acec15a78ac575b5c7e4ba761de10
huichen8 <291222536@qq.com>
2023-08-16 17:51:25 +0800

    support varargs params

--- list/arraylist/array.go
+++ list/arraylist/array.go
@@ -1,42 +1,48 @@
 package arraylist
 
+const DEFAULT_CAP = 32
+
 type ArrayList[T any] struct {
 	elements []T
 }
 
-func New[T any]() *ArrayList[T] {
-	return &ArrayList[T]{
-		elements: make([]T, 0, 32),
+func New[T any](e ...T) *ArrayList[T] {
+	al := &ArrayList[T]{
+		elements: make([]T, 0, DEFAULT_CAP),
 	}
+	al.elements = append(al.elements, e...)
+	return al
 }
 
 func (l *ArrayList[T]) Len() int {
 	return len(l.elements)
 }
 
-func (l *ArrayList[T]) Add(idx int, e T) {
-	if idx < 0 || idx > l.Len() {
-		return
-	}
-	if idx == 0 {
-		l.elements = append([]T{e}, l.elements...)
-		return
-	}
-	if idx == l.Len() {
-		l.elements = append(l.elements, e)
-		return
-	}
-	l.elements = append(l.elements[:idx], append([]T{e}, l.elements[idx:]...)...)
+func (l *ArrayList[T]) Cap() int {
+	return cap(l.elements)
 }
 
-func (l *ArrayList[T]) Set(idx int, e T) T {
+func (l *ArrayList[T]) Append(e ...T) {
+	l.elements = append(l.elements, e...)
+}
+
+func (l *ArrayList[T]) Insert(idx int, e ...T) {
+	if idx < 0 || idx >= l.Len() {
+		l.elements = append(l.elements, e...)
+	} else {
+		l.elements = append(l.elements[:idx], append(e, l.elements[idx:]...)...)
+	}
+}
+
+// return empty of 'T' if out of range list orelse the replaced one
+func (l *ArrayList[T]) Set(idx int, e T) (T, bool) {
 	var el T
 	if idx < 0 || idx >= l.Len() {
-		return el
+		return el, false
 	}
 	el = l.elements[idx]
 	l.elements[idx] = e
-	return el
+	return el, true
 }
 
 func (l *ArrayList[T]) Get(idx int) (T, bool) {
@@ -54,11 +60,19 @@ func (l *ArrayList[T]) Remove(idx int) {
 	l.elements = l.elements[:idx+copy(l.elements[idx:], l.elements[idx+1:])]
 }
 
-// returns position index of element and if present or not
-func (l *ArrayList[T]) Contains(e T, fn func(i, j T) bool) (int, bool) {
-	for idx, el := range l.elements {
-		if fn(e, el) {
-			return idx, true
+func (l *ArrayList[T]) Foreach(fn func(e T)) {
+	for _, e := range l.elements {
+		fn(e)
+	}
+}
+
+// returns first position index of element and if present or not
+func (l *ArrayList[T]) Contains(compare func(i, j T) bool, es ...T) (int, bool) {
+	for _, e := range es {
+		for idx, el := range l.elements {
+			if compare(el, e) {
+				return idx, true
+			}
 		}
 	}
 	return -1, false
